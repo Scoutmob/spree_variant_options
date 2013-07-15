93c4f6bcb7cc964ec5754c159939196f5ab159bb
Eric Vass <ericv@scoutmob.com>
2013-07-15 10:44:41 -0400

    Use display price for variant hash data

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -10,7 +10,7 @@ Spree::Variant.class_eval do
     { 
       :id    => self.id, 
       :count => self.count_on_hand, 
-      :price => number_to_currency(actual_price)
+      :price => actual_price.display_price.to_html
     }
   end
     
