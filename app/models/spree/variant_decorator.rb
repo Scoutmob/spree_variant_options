6aa4bd68fe9cf042b605f586265fb4a824022e7a
Eric Vass <ericv@scoutmob.com>
2013-07-15 10:57:42 -0400

    Use display price in variant hash

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,12 +5,11 @@ Spree::Variant.class_eval do
   attr_accessible :option_values
   
   def to_hash
-    actual_price  = self.price
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
     { 
       :id    => self.id, 
       :count => self.count_on_hand, 
-      :price => actual_price.display_price.to_html
+      :price => display_price.to_html({html: true}.merge({no_cents: true}))
     }
   end
     
