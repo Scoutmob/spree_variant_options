739c3a3239c1df0f42f6998957b5910e1089aa0d
Eric Vass <ericv@scoutmob.com>
2013-07-17 15:33:18 -0400

    Default retail price to product value

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -6,11 +6,12 @@ Spree::Variant.class_eval do
   
   def to_hash
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
+    found_retail_price = !retail_price.nil? ? retail_price : product.retail_price
     { 
       :id    => self.id, 
       :count => self.count_on_hand, 
       :price => display_price.to_html({html: true}.merge({no_cents: true})), 
-      :retail_price => !retail_price.nil? && retail_price.is_a?(Numeric) ? Money.new(retail_price * 100, "USD").format(no_cents: true) : ''
+      :retail_price => found_retail_price.nil? && found_retail_price.is_a?(Numeric) ? Money.new(found_retail_price * 100, "USD").format(no_cents: true) : ''
     }
   end
     
